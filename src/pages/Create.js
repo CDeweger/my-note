b6e38048515a0f4eece96d1118e2ded7c109cc5a
CDeweger <caroljydeweger@gmail.com>
2022-02-07 23:30:19 -0800

    add Typography component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 const Create = () => {
-  return <div>create</div>;
+  return (
+    <div>
+      <Typography
+        varient="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a New Post
+      </Typography>
+    </div>
+  );
 };
 
 export default Create;
