7d08ccee26c6cf6d0efcc41b74ee8a5f26241086
CDeweger <caroljydeweger@gmail.com>
2022-02-08 18:28:16 -0800

    add handleDelete

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { CardHeader, IconButton, Typography } from "@mui/material";
+import { CardContent } from "@mui/material";
+import { Card } from "@mui/material";
+import { DeleteOutlined } from "@mui/icons-material";
 
-const NoteCard = ({ note }) => {
-  return <div>{note.title}</div>;
+const NoteCard = ({ note, handleDelete }) => {
+  return (
+    <div>
+      <Card elevation={1}>
+        <CardHeader
+          action={
+            <IconButton onClick={() => handleDelete(note.id)}>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {note.details}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default NoteCard;
