6c39f2fcc44c9cad6594d227df5c46a8824bd39a
CDeweger <caroljydeweger@gmail.com>
2022-02-08 21:54:21 -0800

    add conditional rendering for NoteCard

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,63 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
-import { Drawer } from "@mui/material";
+import {
+  Drawer,
+  ListItem,
+  ListItemIcon,
+  List,
+  ListItemText,
+} from "@mui/material";
 import { Typography } from "@mui/material";
+import { AddCircleOutlined, SubjectOutlined } from "@mui/icons-material";
+import {
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { purple } from "@mui/material/colors";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
-  drawer: {
-    width: drawerWidth,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  root: {
-    display: "flex",
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: "flex",
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(3),
+    },
+  };
 });
 
 const Layout = ({ children }) => {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const menuItem = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
 
   return (
     <div className={classes.root}>
@@ -33,8 +68,24 @@ const Layout = ({ children }) => {
         className={classes.drawer}
       >
         <div>
-          <Typography variant="h5">My Notes</Typography>
+          <Typography variant="h5" className={classes.title}>
+            My Notes
+          </Typography>
         </div>
+
+        <List>
+          {menuItem.map((item) => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText>{item.text}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -3,11 +3,23 @@ import { CardHeader, IconButton, Typography } from "@mui/material";
 import { CardContent } from "@mui/material";
 import { Card } from "@mui/material";
 import { DeleteOutlined } from "@mui/icons-material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  test: {
+    border: (note) => {
+      if (note.category == "work") {
+        return "1px solid red";
+      }
+    },
+  },
+});
 
 const NoteCard = ({ note, handleDelete }) => {
+  const classes = useStyles(note);
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
